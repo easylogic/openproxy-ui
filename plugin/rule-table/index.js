aa50ba47df1e082cdb12937bae26e4844f77e382
jinho park <cyberuls@gmail.com>
2016-11-29 22:25:11 +0900

    add plugin system

--- plugin/rule-table/index.js
+++ plugin/rule-table/index.js
@@ -12,7 +12,32 @@ class RuleTable {
     }
 
     initEvent() {
+        $(".policy-items").on("click", ".check-status", function (e) {
+            e.preventDefault();
+            if ($(this).hasClass('icon-checkbox')) {
+                $(this).addClass('icon-checkbox2').removeClass('icon-checkbox');
+            } else {
+                $(this).addClass('icon-checkbox').removeClass('icon-checkbox2');
+            }
+        });
+        $(".policy-items").on("click", ".icon-trashcan", function (e) {
+            if (confirm("Delete a rule?")) {
+                $(this).parent().parent().remove();
+            }
+            e.preventDefault();
+            return;
+        })
+        $(".policy-items").on("click", "> .panel .head", function (e) {
 
+            if ($(e.target)[0].tagName == 'I') {
+
+            } else {
+                $(".policy-items .panel.selected").removeClass('selected');
+                $(this).parent().addClass('selected');
+            }
+
+
+        })
     }
 
     destroy () {
@@ -24,7 +49,7 @@ class RuleTable {
     }
 
     refresh () {
-        
+
     }
 
     render () {
--- renderer.js
+++ renderer.js
@@ -21,30 +21,5 @@ jui.ready(function () {
         createPanel(PluginObject, PluginClass);
     });
 
-    $(".policy-items").on("click", ".check-status", function (e) {
-        e.preventDefault();
-        if ($(this).hasClass('icon-checkbox')) {
-            $(this).addClass('icon-checkbox2').removeClass('icon-checkbox');
-        } else {
-            $(this).addClass('icon-checkbox').removeClass('icon-checkbox2');
-        }
-    });
-    $(".policy-items").on("click", ".icon-trashcan", function (e) {
-        if (confirm("Delete a rule?")) {
-            $(this).parent().parent().remove();
-        }
-        e.preventDefault();
-        return;
-    })
-    $(".policy-items").on("click", "> .panel .head", function (e) {
-
-        if ($(e.target)[0].tagName == 'I') {
-
-        } else {
-            $(".policy-items .panel.selected").removeClass('selected');
-            $(this).parent().addClass('selected');
-        }
-
-
-    })
+   
 });
