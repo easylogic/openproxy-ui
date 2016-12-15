e932e4495a6d6d4eafac857318c8731064a079f4
jinho park <cyberuls@gmail.com>
2016-12-15 15:59:50 +0900

    implements windows installer

---
+++ installers/windows/installer.js
@@ -0,0 +1,22 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+    .then(createWindowsInstaller)
+    .catch((error) => {
+        console.error(error.message || error)
+        process.exit(1)
+    })
+
+function getInstallerConfig () {
+    console.log('creating windows installer')
+    const rootPath = path.join('./')
+    const outPath = path.join(rootPath, 'release-builds')
+
+    return Promise.resolve({
+        appDirectory: path.join(outPath, 'openproxy'),
+        authors: 'easylogic',
+        outputDirectory: path.join(outPath, 'windows-installer'),
+        exe: 'openproxy.exe'
+    })
+}
